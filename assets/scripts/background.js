a6836818241ec5368f2bacbc69f10e13115e6aab
Prince <prince@deriv.com>
2021-08-26 17:23:52 +0400

    Fix: content script unable to pickup environment change

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -4,43 +4,46 @@ const storage = chrome.storage.local;
 
 let script = null;
 
-// Environment handler
-storage.get(env_key, function (value) {
-  const val = value[env_key];
-
-  if (val != undefined) {
-    window.env = val;
-  }
-
-  setInterval(() => fetchUpdate(), 3000);
-});
-
 const fetchUpdate = () => {
-  // Append Extension Renderer Script
-  const url =
-    window.env == "local"
-      ? "http://localhost:4444"
-      : "https://prince-deriv.github.io/dtools-production";
-  const time = Math.floor(new Date().getTime() / 1000);
-  const xhr = new XMLHttpRequest();
-  const full_url = `${url}/assets/scripts/renderer/content-obs.js?v=${time}`;
-  xhr.open("GET", full_url, true);
+  // Environment handler
+  storage.get(env_key, function (value) {
+    const val = value[env_key];
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-      const response = xhr.responseText;
-
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {
-          action: "eval",
-          data: {
-            script: response,
-          },
-        });
-      });
+    if (val != undefined) {
+      window.env = val;
     }
-  };
 
-  xhr.send();
+    // Append Extension Renderer Script
+    const url =
+      window.env == "local"
+        ? "http://localhost:4444"
+        : "https://prince-deriv.github.io/dtools-production";
+    const time = Math.floor(new Date().getTime() / 1000);
+    const xhr = new XMLHttpRequest();
+    const full_url = `${url}/assets/scripts/renderer/content-obs.js?v=${time}`;
+    xhr.open("GET", full_url, true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        const response = xhr.responseText;
+
+        chrome.tabs.query(
+          { currentWindow: true, active: true },
+          function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {
+              action: "eval",
+              data: {
+                script: response,
+              },
+            });
+          }
+        );
+      }
+    };
+
+    xhr.send();
+  });
 };
+
+setInterval(() => fetchUpdate(), 3000);
